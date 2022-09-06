7efd9600035893dec835615630cf8754a079dd54
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2022-09-06 10:58:22 +0300

    Update sortnumber.java

--- sortnumber.java
+++ sortnumber.java
@@ -16,15 +16,16 @@ public class Main {
 			else System.out.println(a+"<"+c+"<"+b);
 		}
 		
-		if((b < a) && (b < c)) {
+		else if((b < a) && (b < c)) {
 			if(a < c) System.out.println(b+"<"+a+"<"+c);
 			else System.out.println(b+"<"+c+"<"+a);
 		}
 		
-		if((c < b) && (c < a)) {
+		else{
 			if(b < a) System.out.println(c+"<"+b+"<"+a);
 			else System.out.println(c+"<"+a+"<"+b);
 		}
+
 		
 		
 	}
