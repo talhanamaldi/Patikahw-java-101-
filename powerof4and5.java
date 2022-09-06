d6a0915c8a9a5c94b75a10cd63944c87e79644c6
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2022-09-06 19:52:00 +0300

    Update powerof4and5.java

--- powerof4and5.java
+++ powerof4and5.java
@@ -6,11 +6,11 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		
-		for(int i = 1; i < n; i*=4) {
+		for(int i = 1; i <= n; i*=4) {
 			System.out.print(i+" ");
 		}
 		System.out.println();
-		for(int i = 1; i < n; i*=5) {
+		for(int i = 1; i <= n; i*=5) {
 			System.out.print(i+" ");
 		}
 
