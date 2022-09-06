1dc5bcbf99532e7bb917d04462b130171080d4a9
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2022-09-06 21:17:16 +0300

    Create ebobekok.java

---
+++ ebobekok.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Birinci sayı : ");
+		int n1 = scan.nextInt();
+		System.out.println("İkinci sayı : ");
+		int n2 = scan.nextInt();
+		int ebob = 0;
+		
+		int counter = 1;
+		while(counter < Math.max(n1, n2)) {
+			if(n1 % counter == 0) {
+			if(n2 % counter == 0) ebob = counter;
+			}
+			counter++;
+		}
+		int ekok = (n1*n2)/ebob;
+		System.out.println("Sayıların ebobu = "+ebob+"\nSayıların ekoku = "+ekok);	
+	}
+}
