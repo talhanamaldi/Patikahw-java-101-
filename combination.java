a37c029d22663561d80be3eece6fe5914a8156d0
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2022-09-06 20:03:38 +0300

    Create combination.java

---
+++ combination.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+        System.out.println("n degerini giriniz: ");
+        int n = scan.nextInt();
+        System.out.println("r degerini giriniz: ");
+        int r = scan.nextInt();
+                
+        double combination = (factorial(n)*1.0)/(factorial(r)*factorial(n-r));
+        
+        System.out.println(combination);
+
+	}
+	
+	static int factorial(int n) {
+		if(n == 0) return 1;
+		else return(n*factorial(n-1));
+	}
+
+}
