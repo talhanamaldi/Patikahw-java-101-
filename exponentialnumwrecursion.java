3b32fe8794a4e10826a969fe9ce2d06c4bcf52cd
Talha Namaldı <69728377+talhanamaldi@users.noreply.github.com>
2022-09-11 12:41:59 +0300

    Create exponentialnumwrecursion.java

---
+++ exponentialnumwrecursion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Taban : ");
+		int base = scan.nextInt();
+		System.out.print("Üs : ");
+		int power = scan.nextInt();
+		
+		System.out.println(exponential(base, power));
+
+	}
+	
+	public static int exponential(int n, int m) {
+		if(m == 1) return n;
+		else return n*exponential(n, m-1);
+	}
+	
+
+}
